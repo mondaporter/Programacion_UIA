dea50e1788fc52eec199e627340ee4cd12df71c0
mondaporter <66339297+mondaporter@users.noreply.github.com>
2022-01-16 23:02:34 -0600

    1. Hola Mundo
    15.01.2022

---
+++ Hola Mundo/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Las clases siempre comenzaran con mayusculas, mientras las variables en
+        // minusculas.
+
+        System.out.println("Hola Mundo!");
+    }
+}
